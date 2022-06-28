0cf811e2a827c3b48ff531a498e9dfc1ec5f63ff
Gabriel Gutierrez <gg447062@gmail.com>
2022-06-28 12:49:57 -0500

    refactor to screenshot on data loaded

--- app/Components/App.js
+++ app/Components/App.js
@@ -8,46 +8,33 @@ const client = new GraphQLClient({
 });
 
 const App = () => {
-  const [skip, setSkip] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
   const screenshot = () => {
     const screenshotTarget = document.body;
-    html2canvas(screenshotTarget).then((canvas) => {
+    html2canvas(screenshotTarget, { scale: 2 }).then((canvas) => {
       const date = new Date().toLocaleDateString();
       const base64image = canvas.toDataURL('image/png');
       const a = document.createElement('a');
       a.setAttribute('href', base64image);
       a.download = `jbx-snapshot-${date}.png`;
-      setTimeout(() => {
-        a.click();
-        a.remove();
-      }, 1000);
+      a.click();
+      a.remove();
     });
   };
 
-  const handleClick = () => {
-    setSkip(skip + 10);
-    screenshot();
-  };
-
   useEffect(() => {
-    document.body.addEventListener('click', handleClick);
-    screenshot();
-    return () => {
-      document.body.removeEventListener('click', handleClick);
-    };
-  }, []);
+    if (loaded) {
+      screenshot();
+    }
+  }, [loaded]);
 
   return (
     <ClientContext.Provider value={client}>
       <main>
         <header>
           <div>
-            <img
-              src="assets/fake_qr.png"
-              alt="qr code"
-              style={{ width: '10em' }}
-            />
+            <img src="assets/qr.png" alt="qr code" style={{ width: '10em' }} />
             <a href="https://snapshot.org/#/jbdao.eth" target={'_blank'}>
               <img
                 src="assets/jbx_logo_screenshot.png"
@@ -58,12 +45,10 @@ const App = () => {
           </div>
           <h1>JuiceboxDAO Open Proposals</h1>
         </header>
-        <Proposals skip={skip} />
+        <Proposals setLoaded={setLoaded} />
       </main>
       <div className="button-wrapper">
-        <div className="view-more" onClick={handleClick}>
-          Click Anywhere to View More
-        </div>
+        <div className="view-more">Click Anywhere to View More</div>
       </div>
     </ClientContext.Provider>
   );
--- app/Components/Proposals.js
+++ app/Components/Proposals.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from 'graphql-hooks';
 
-const PROPSALS_QUERY = `query Proposals($skip: Int) {
+const OPEN_PROPOSALS = `query Proposals {
   proposals (
-    first: 10,
-    skip: $skip,
+    first:10,
+    skip:0,
     where: {
       space_in: ["jbdao.eth"],
       state: "active"
@@ -15,47 +15,46 @@ const PROPSALS_QUERY = `query Proposals($skip: Int) {
     id
     title
 
-    start
-    end
-    snapshot
-
   }
 }`;
 
-const Proposals = ({ skip }) => {
-  const { loading, error, data } = useQuery(PROPSALS_QUERY, {
-    variables: { skip: skip },
-  });
+const Proposals = ({ setLoaded }) => {
+  const { loading, error, data } = useQuery(OPEN_PROPOSALS);
 
-  if (loading) return 'loading...';
-
-  if (error) return 'an error occured';
-
+  useEffect(() => {
+    if (data && !loading) {
+      setLoaded(true);
+    }
+  }, [data, loading]);
   return (
     <div className="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <td>JBP</td>
-            <td>TITLE</td>
-            <td>START DATE</td>
-            <td>END DATE</td>
-          </tr>
-        </thead>
+      {error && <div className="message">an error ocurred...</div>}
+      {loading && <div className="message">loading...</div>}
+      {data && (
+        <table>
+          <thead>
+            <tr>
+              <td>JBP</td>
+              <td>TITLE</td>
+              <td>START DATE</td>
+              <td>END DATE</td>
+            </tr>
+          </thead>
 
-        <tbody>
-          {data.proposals.map((el, i) => {
-            return (
-              <tr key={i}>
-                <td>{el.title.slice(0, 7)}</td>
-                <td>{el.title.slice(10, el.title.length)}</td>
-                <td>{new Date(el.start * 1000).toDateString()}</td>
-                <td>{new Date(el.end * 1000).toDateString()}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          <tbody>
+            {data.proposals.map((el, i) => {
+              return (
+                <tr key={i}>
+                  <td>{el.title.slice(0, 7)}</td>
+                  <td>{el.title.slice(10, el.title.length)}</td>
+                  <td>{new Date(el.start * 1000).toDateString()}</td>
+                  <td>{new Date(el.end * 1000).toDateString()}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
